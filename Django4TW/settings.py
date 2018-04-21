13155b360ce1605bf07e3b6c3706aecf48414d88
lacosmok <lacosmok@gmail.com>
2018-04-21 14:08:02 +0200

    feat: add, package.json, change css files to less files, changes to html template

--- Django4TW/settings.py
+++ Django4TW/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Django4TW.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['static/html'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
